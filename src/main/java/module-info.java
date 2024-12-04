61bff7a72e3231e2ee5ce4d489c54e1bbc049bf4
Никита Кораблев <kit06092003@gmail.com>
2024-12-05 00:06:33 +0300

    deleted unused folders

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,6 @@ module com.gomoku.gomoku_game_rmi {
     requires com.dlsc.formsfx;
     requires com.almasb.fxgl.all;
     requires java.rmi;
-    requires io.reactivex.rxjava3;
     requires java.desktop;
 
     // Base Gomoku Game
@@ -19,13 +18,4 @@ module com.gomoku.gomoku_game_rmi {
     exports com.gomoku.gomoku_game_rmi.client;
     exports com.gomoku.gomoku_game_rmi.global_objects.enums;
 
-    // Gomoku Game on RMI
-    opens com.gomoku.gomoku_game_grpc to javafx.fxml;
-    exports com.gomoku.gomoku_game_grpc;
-    exports com.gomoku.gomoku_game_grpc.global_objects;
-    exports com.gomoku.gomoku_game_grpc.global_objects.intefaces;
-    opens com.gomoku.gomoku_game_grpc.global_objects.intefaces to javafx.fxml;
-    opens com.gomoku.gomoku_game_grpc.client to javafx.fxml;
-    exports com.gomoku.gomoku_game_grpc.client;
-    exports com.gomoku.gomoku_game_grpc.global_objects.enums;
 }
